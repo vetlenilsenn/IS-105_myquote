3880828711809e949a604be63d5acf7e39ef056e
vetlenilsenn <vetle.nilsen@live.no>
2022-01-27 12:20:54 +0100

    Second commit

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
        "fmt"
-       myquote_pakke "github.com/vetlenilsenn/IS-105_myquote"
+       myquote_pakke "github.com/vetlenilsenn/IS-105_myquote/tree/master/myquote_pakke"
 )
 
 func main() {
