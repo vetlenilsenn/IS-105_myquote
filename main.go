e341c9b2755bd398773d12f936379947c0a09a24
vetlenilsenn <vetle.nilsen@live.no>
2022-01-27 12:18:52 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+       "fmt"
+       myquote_pakke "github.com/vetlenilsenn/IS-105_myquote"
+)
+
+func main() {
+    fmt.Println(myquote_pakke.Hello())
+		fmt.Println(myquote_pakke.Opt())
+		fmt.Println(myquote_pakke.Glass())
+		fmt.Println(myquote_pakke.Go())
+}
---
+++ myquote_pakke/myquote_pakke.go
@@ -0,0 +1,20 @@
+package myquote_pakke
+
+import "rsc.io/quote"
+
+
+func Hello() string {
+	return quote.Hello()
+}
+
+func Glass() string {
+	return quote.Glass()
+}
+
+func Go() string {
+	return quote.Go()
+}
+
+func Opt() string {
+	return quote.Opt()
+}
